73bb938a7772c980206ff1be852d4d301255094c
Jake White <jwhite52@gmail.com>
2021-10-02 19:09:07 -0400

    Implement react-bootstrap components

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,19 @@
 import * as React from 'react'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Button from 'react-bootstrap/Button'
+
+import './navBar.css'
 
 const NavBar = ({selectedPage}) => {
   return (
     <div>
-      <p>Hello World {selectedPage}</p>
+    <h1>Navigation Bar</h1>
+      <ButtonToolbar className="custom-btn-toolbar">
+        <Button>{selectedPage}</Button>
+        <Button>Photos</Button>
+      </ButtonToolbar>
     </div>
   )
 }
+
 export default NavBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import 'bootstrap/dist/css/bootstrap.min.css';
 // styles
-
 
 // markup
 const IndexPage = () => {

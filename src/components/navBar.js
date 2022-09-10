d0e945dafc87a963919e1c9b7bf822dbf076dca1
Jake White <jwhite52@gmail.com>
2022-09-09 22:04:59 -0400

    Change Navbar Bootstrap links to Gatsby links

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,21 +2,19 @@ import * as React from 'react'
 // import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 // import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import DLWLogo from '../images/dlwlogo.jpeg'
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 
 
 import './navBar.css'
 
-const NavBar = ({selectedPage}) => {
+const NavBar = () => {
   return (
     <div>
-      <h1>{selectedPage}</h1>
       <Navbar className="nav-bar" variant="dark">
           <Container>
-            <Navbar.Brand href="/">
+            <Link to ="/">
               <img
                 alt=""
                 src={DLWLogo}
@@ -24,9 +22,10 @@ const NavBar = ({selectedPage}) => {
                 height="30"
                 className="d-inline-block align-top"
               />{' '}
-            Home
-            </Navbar.Brand>
-            <Nav.Link href="/photos">Photos</Nav.Link>
+            </Link>
+            <Link to="/photos">
+              <span>Photos</span>
+            </Link>
           </Container>
         </Navbar>
     </div>
